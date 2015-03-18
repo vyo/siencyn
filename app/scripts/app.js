03b192aaf922145037e4e8938392522dfa5f585b
Manuel Weidmann <weidmann.manuel@gmail.com>
2015-03-18 21:53:37 +0100

    Add angular scaffold

---
+++ app/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name siencynApp
+ * @description
+ * # siencynApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('siencynApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .when('/curiad', {
+        templateUrl: 'views/curiad.html',
+        controller: 'CuriadCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
